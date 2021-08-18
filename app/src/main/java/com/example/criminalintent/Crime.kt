2e0787511ad67c1ad44384ee2447af13b6afa08d
Shafique <shafiqued17z@gmail.com>
2021-08-18 23:48:03 +0500

    Room Database and CrimeRepository and more.

--- app/src/main/java/com/example/criminalintent/Crime.kt
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -1,12 +1,23 @@
 package com.example.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 import java.util.UUID
 
 //Data class to hold data for each crime w/ properties, ID, title, date, isSolved.
+
+/**@Entity annotation indicates that this class defines the structure of a table.
+ *
+ * Each property defined on the class will be a column in the table. *property name == column name.*
+ * This table will have 4 columns as indicated by properties. id, title, date, and isSolved.
+ *
+ * @PrimaryKey annotation specifies which column in your database is the primary key.
+ * */
+@Entity
 data class Crime(
-    val id: UUID = UUID.randomUUID(),
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
     var title: String = "",
     var date: Date = Date(),
-    var isSolved: Boolean = false) {
-}
+    var isSolved: Boolean = false
+)
